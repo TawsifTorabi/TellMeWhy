5b28ec6befe815f645a42e404413105dd2fd13cf
Tawsif Torabi <tawsiftorabi.jr@gmail.com>
2022-05-07 23:37:32 +0600

    Update script.js

--- script.js
+++ script.js
@@ -12,8 +12,28 @@
 (function() {
     'use strict';
 
+    //Modificable Variables
+    var AddBalance = 5000;
+
+
+    var OldBalanceString = document.getElementById('ctl00_MainContainer_FI_CurrentBalance').innerHTML;
+    var filteredBalanceString = OldBalanceString.replace(" Tk.", "");
+    var OldBalance = numberWithoutCommas(filteredBalanceString);
+    var NewBalance = OldBalance + AddBalance;
+
+    //Number Comma Separator
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    //Number Comma Separator remover
+    function numberWithoutCommas(x) {
+        return parseFloat(x.replace(/,/g, ''));
+    }
+
+
     //Homepage Account Balance Check
-    document.getElementById('ctl00_MainContainer_FI_CurrentBalance').innerHTML = '70,000 Tk.';
+    document.getElementById('ctl00_MainContainer_FI_CurrentBalance').innerHTML = numberWithCommas(NewBalance)+' Tk.';
 
 
 })();
